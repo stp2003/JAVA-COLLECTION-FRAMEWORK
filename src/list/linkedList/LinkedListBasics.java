ea8e0f1c488c7f42836b2a501167e59926c66a9c
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-05-18 19:05:39 +0530

    linked list basics

---
+++ src/list/linkedList/LinkedListBasics.java
@@ -0,0 +1,15 @@
+package list.linkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListBasics {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
